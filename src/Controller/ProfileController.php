30b9fc19c9c53d2d9b060ef60f2db9dccffec243
Unknown <gauthier.axel.lory@gmail.com>
2019-02-06 22:13:30 +0100

    création interface user

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\ProfileType;
+use Doctrine\Common\Persistence\ObjectManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ProfileController
+ * @package App\Controller
+ * @IsGranted("ROLE_USER")
+ */
+class ProfileController extends AbstractController
+{
+    /**
+     * @Route("/profile", name="profile")
+     */
+    public function index()
+    {
+        $user = $this->getUser();
+        return $this->render('profile/index.html.twig', [
+            'controller_name' => 'ProfileController',
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * @Route("/profile/account/{id}", name="profile_account")
+     */
+    public function account(User $user, Request $request, ObjectManager $manager)
+    {
+        $user = $this->getUser();
+        $form =$this->createForm(ProfileType::class, $user);
+        $form->handleRequest($request);
+
+        return $this->render('profile/account.html.twig', [
+            'controller_name' => 'ProfileController',
+            'user' => $user,
+            'form' => $form->createView()
+        ]);
+    }
+}
