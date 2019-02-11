f7f91e1965d2ea315824eabec1bfb0d8bb482d1f
ogpoggi <bastienpoggi@msn.com>
2019-02-11 23:50:37 +0100

    add js jquery

---
+++ assets/js/review.js
@@ -0,0 +1,28 @@
+ // loads the jquery package from node_modules
+var $ = require('jquery');
+
+ // import the function from greet.js (the .js extension is optional)
+var greet = require('./greet');
+
+/*var $ratingscore = $("#ratingscore");
+$ratingscore.on("change", function () {
+    if ($client.val() != "") {
+        $.ajax({"type": "GET",
+            "url": "/component/chronoforms5/?chronoform=saisie_bt&event=ajax_agences&tvout=ajax",
+            "data": $client.serialize(),
+            "success": function (res) {
+                var $agence = $("#agence");
+                var prev_val = $agence.val();
+                $agence.find("option").remove();
+                $.each($.parseJSON(res), function (id, val) {
+                    var html = '';
+                    if (prev_val == val) {
+                        html = 'selected=selected';
+                    }
+                    html = '<option ' + html + ' value="' + id + '">' + val + '</option>';
+                    $agence.append(html);
+                });
+            } // success
+        }); // ajax
+    } // if
+});*/
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
+
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
@@ -17,7 +18,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
-    .addEntry('app', './build/js/app.js')
+    .addEntry('app', './assets/js/app.js')
+    .addEntry('review', './assets/js/review.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
@@ -45,7 +47,7 @@ Encore
     //.enableTypeScriptLoader()
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .autoProvidejQuery()
 
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
