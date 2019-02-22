2dfedc5ca1875534e28c17f760884bc141204b3c
Unknown <gauthier.axel.lory@gmail.com>
2019-02-22 21:48:27 +0100

    intégration de CKeditor

--- src/Form/ReviewType.php
+++ src/Form/ReviewType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Product;
 use App\Entity\Review;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,7 +15,11 @@ class ReviewType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('description')
+            ->add('description', CKEditorType::class, array(
+                'config' => array(
+                    'toolbar' => 'basic'
+                )
+            ))
             ->add('rating')
         ;
     }
