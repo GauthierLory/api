7938b7a4101cf604c74e88f329ec5f43f3b703b2
ogpoggi <bastienpoggi@msn.com>
2019-03-28 14:13:16 +0100

    Delete nav.js

--- assets/js/nav.js
+++
@@ -1,9 +0,0 @@
-$(window).on('scroll', function () {
-    var scrollTop     = $(window).scrollTop(),
-        elementOffset = $('#navogp').offset().top,
-        distance      = (elementOffset - scrollTop);
-
-    if(distance == 0){
-        //Fair eles changements CSS
-    }
-});
